a7a2f66795b56b1f7238415b4492ea2187c330ba
mhahnenberg@mozilla.com <mhahnenberg@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-07-13 23:44:16 +0000

    Ditto.
    git-svn-id: https://svn.mozilla.org/projects/cesium/trunk/cesium@29702 4eb1ac78-321c-0410-a911-ec516a8615a5

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ ${BROWSER_PATH} -CreateProfile "cesium"
 cp install/user.js ${HOME}/.mozilla/firefox/*.cesium/user.js
 
 # install add-ons
+mkdir ${HOME}/.mozilla/firefox/*.cesium/extensions
 mkdir install/firebug
 unzip install/firebug-1.3.3-fx.xpi -d install/firebug
 EXT_PATH=`grep "em:id" install/firebug/install.rdf | grep "@" | cut -d">" -f2 | cut -d"<" -f1`
