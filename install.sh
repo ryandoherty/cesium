b8e25ad50c9df0c2e04fb6015f8d321b42cd5b1b
mhahnenberg@mozilla.com <mhahnenberg@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-07-13 23:41:08 +0000

    Install script fixes.
    git-svn-id: https://svn.mozilla.org/projects/cesium/trunk/cesium@29701 4eb1ac78-321c-0410-a911-ec516a8615a5

--- install.sh
+++ install.sh
@@ -6,16 +6,16 @@ BROWSER_PATH=/usr/bin/firefox
 ${BROWSER_PATH} -CreateProfile "cesium"
 
 # copy prefs to profile
-cp user.js ${HOME}/.mozilla/firefox/*.cesium/user.js
+cp install/user.js ${HOME}/.mozilla/firefox/*.cesium/user.js
 
 # install add-ons
-mkdir firebug
-unzip firebug-1.3.3-fx.xpi -d firebug
-EXT_PATH=`grep "em:id" firebug/install.rdf | grep "@" | cut -d">" -f2 | cut -d"<" -f1`
-mv firebug ${HOME}/.mozilla/firefox/*.cesium/extensions/${EXT_PATH}
+mkdir install/firebug
+unzip install/firebug-1.3.3-fx.xpi -d install/firebug
+EXT_PATH=`grep "em:id" install/firebug/install.rdf | grep "@" | cut -d">" -f2 | cut -d"<" -f1`
+mv install/firebug ${HOME}/.mozilla/firefox/*.cesium/extensions/${EXT_PATH}
 
 
-mkdir yslow
-unzip yslow-2.0.0b4-fx.xpi -d yslow 
-EXT_PATH=`grep "em:id" yslow/install.rdf | grep "@" | cut -d">" -f2 | cut -d"<" -f1`
-mv yslow ${HOME}/.mozilla/firefox/*.cesium/extensions/${EXT_PATH}
+mkdir install/yslow
+unzip install/yslow-2.0.0b4-fx.xpi -d install/yslow 
+EXT_PATH=`grep "em:id" install/yslow/install.rdf | grep "@" | cut -d">" -f2 | cut -d"<" -f1`
+mv install/yslow ${HOME}/.mozilla/firefox/*.cesium/extensions/${EXT_PATH}
