d2969c43d66bcfe8ee01e88af6746d14cc653122
mhahnenberg@mozilla.com <mhahnenberg@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-07-13 23:54:33 +0000

    Ditto.
    git-svn-id: https://svn.mozilla.org/projects/cesium/trunk/cesium@29704 4eb1ac78-321c-0410-a911-ec516a8615a5

--- install.sh
+++ install.sh
@@ -5,18 +5,22 @@ BROWSER_PATH=/usr/bin/firefox
 # create profile
 ${BROWSER_PATH} -CreateProfile "cesium"
 
+# get random char string at front of profile folder name
+CHAR_STR=`ls ${HOME}/.mozilla/firefox | grep "cesium" | cut -d"." -f1`
+PROF_PATH=${HOME}/.mozilla/firefox/${CHAR_STR}.cesium
+
 # copy prefs to profile
-cp install/user.js ${HOME}/.mozilla/firefox/*.cesium/user.js
+cp install/user.js ${PROF_PATH}/user.js
 
 # install add-ons
-mkdir ${HOME}/.mozilla/firefox/*.cesium/extensions
+mkdir ${PROF_PATH}/extensions
 mkdir install/firebug
 unzip install/firebug-1.3.3-fx.xpi -d install/firebug
 EXT_PATH=`grep "em:id" install/firebug/install.rdf | grep "@" | cut -d">" -f2 | cut -d"<" -f1`
-mv install/firebug ${HOME}/.mozilla/firefox/*.cesium/extensions/${EXT_PATH}
+mv install/firebug ${PROF_PATH}/extensions/${EXT_PATH}
 
 
 mkdir install/yslow
 unzip install/yslow-2.0.0b4-fx.xpi -d install/yslow 
 EXT_PATH=`grep "em:id" install/yslow/install.rdf | grep "@" | cut -d">" -f2 | cut -d"<" -f1`
-mv install/yslow ${HOME}/.mozilla/firefox/*.cesium/extensions/${EXT_PATH}
+mv install/yslow ${PROF_PATH}/extensions/${EXT_PATH}
