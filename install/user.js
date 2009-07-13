0e340ebf9949b0b2ac9c645ad95e214477e7d547
mhahnenberg@mozilla.com <mhahnenberg@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-07-13 22:22:38 +0000

    First commit for 0.2 making installation easier.
    git-svn-id: https://svn.mozilla.org/projects/cesium/trunk/cesium@29688 4eb1ac78-321c-0410-a911-ec516a8615a5

---
+++ install/user.js
@@ -0,0 +1,5 @@
+user_pref('extensions.yslow.beaconUrl', 
+    'http://localhost:8000/beacon');
+user_pref('extensions.yslow.autorun', true);
+user_pref('extensions.yslow.optinBeacon', true);
+user_pref('browser.sessionstore.enabled', false);
