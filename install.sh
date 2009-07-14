d30de7195a701e4219438053f2427cf75eae6288
mhahnenberg@mozilla.com <mhahnenberg@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-07-14 00:35:26 +0000

    Python environment installation added to install script.
    git-svn-id: https://svn.mozilla.org/projects/cesium/trunk/cesium@29711 4eb1ac78-321c-0410-a911-ec516a8615a5

--- install.sh
+++ install.sh
@@ -18,12 +18,32 @@ cp install/user.js ${PROF_PATH}/user.js
 echo "--> Installing Firebug..."
 mkdir ${PROF_PATH}/extensions
 mkdir install/firebug
-unzip install/firebug-1.3.3-fx.xpi -d install/firebug > /dev/null
+unzip -q install/firebug-1.3.3-fx.xpi -d install/firebug
 EXT_PATH=`grep "em:id" install/firebug/install.rdf | grep "@" | cut -d">" -f2 | cut -d"<" -f1`
 mv install/firebug ${PROF_PATH}/extensions/${EXT_PATH}
 
 echo "--> Installing YSlow..."
 mkdir install/yslow
-unzip install/yslow-2.0.0b4-fx.xpi -d install/yslow > /dev/null
+unzip -q install/yslow-2.0.0b4-fx.xpi -d install/yslow
 EXT_PATH=`grep "em:id" install/yslow/install.rdf | grep "@" | cut -d">" -f2 | cut -d"<" -f1`
 mv install/yslow ${PROF_PATH}/extensions/${EXT_PATH}
+
+# setup Python environment
+echo "--> Installing setuptools..."
+sh install/setuptools-0.6c9-py2.6.egg
+
+echo "--> Installing virtualenv..."
+easy_install virtualenv
+
+echo "--> Setting up virtual environment..."
+virtualenv --no-site-packages virtual_env
+echo "--> Running virtual environment..."
+source virtual_env/bin/activate
+
+echo "--> Installing pip..."
+easy_install pip
+
+echo "--> Installing pip requirements file..."
+pip install -r install/dev-req.txt
+
+echo "--> Done!"
